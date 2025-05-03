c92c2f094fc564d07e4f06e099957e99d1bfa75f
Gisele-123 <giselemigisha53@gmail.com>
2025-05-03 16:08:46 +0200

    project initialization with all dependencies

---
+++ src/main/java/com/rra/vehicletracking/VehicleTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rra.vehicletracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleTrackingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleTrackingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rra/vehicletracking/VehicleTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rra.vehicletracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleTrackingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
